363c958c50f7c578d0728f8f389ae5a8529670f6
KSJ0128 <ksj01128@naver.com>
2024-12-13 17:24:19 +0900

    chore: 프로젝트 세팅

---
+++ Back/src/main/java/kdt/advator/AdvatorApplication.java
@@ -0,0 +1,13 @@
+package kdt.advator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvatorApplication.class, args);
+    }
+
+}
---
+++ Back/src/test/java/kdt/advator/AdvatorApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.advator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
