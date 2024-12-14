7a271bb99d96fbb97446222708b86ce27ea10c69
KSJ0128 <ksj01128@naver.com>
2024-12-14 19:56:32 +0900

    chore: 다중 데이터 소스 사용 보류
    빠른 개발을 위해 다중 데이터 소스 사용을 임시 보류합니다.

--- Back/src/main/java/kdt/advator/config/AdVatorJpaConfig.java
+++ Back/src/main/java/kdt/advator/config/AdVatorJpaConfig.java
@@ -1,12 +1,11 @@
-package kdt.advator.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(
-        basePackages = "kdt.advator.advator",
-        transactionManagerRef = "advatorTransactionManager"
-)
-public class AdVatorJpaConfig {
-}
+//package kdt.advator.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = "kdt.advator.advator"
+//)
+//public class AdVatorJpaConfig {
+//}
