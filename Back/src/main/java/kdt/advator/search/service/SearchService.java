b220e0f0cfc74ccc64364c5bf02bb28f343e67c0
KSJ0128 <ksj01128@naver.com>
2024-12-15 03:49:57 +0900

    feat: 지도 건물 조회 - gps : 서비스 로직
    지도 건물 조회 - gps API 구현을 위한 서비스 로직을 구현했습니다.
    gpsX, gpsY, sort, rating, company 등의 값을 통해 아파트 리스트를 조회 가능합니다.

--- Back/src/main/java/kdt/advator/search/service/SearchService.java
+++ Back/src/main/java/kdt/advator/search/service/SearchService.java
@@ -6,6 +6,7 @@ import kdt.advator.common.repository.ApartRepository;
 import kdt.advator.estimate.domain.Estimate;
 import kdt.advator.estimate.service.EstimateService;
 import kdt.advator.search.dto.SearchApartDTO;
+import kdt.advator.search.dto.SearchGpsDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -58,4 +59,34 @@ public class SearchService {
                 .findFirst()
                 .orElse(null); // Estimate가 없을 경우 null 반환
     }
+
+    public List<ApartDTO> getApartByGps(SearchGpsDTO searchGpsDTO) {
+        List<Apart> apartList = apartRepository.findByGpsXAndGpsY(searchGpsDTO.getGpsX(), searchGpsDTO.getGpsY(), searchGpsDTO.getRating(), searchGpsDTO.getCompany());
+
+        if (apartList.isEmpty())
+            return new ArrayList<>();
+
+        List<Estimate> estimateList = estimateService.getEstimateList(apartList);
+        if (estimateList.isEmpty())
+            return new ArrayList<>();
+
+        // DTO 리스트 생성 및 정렬
+        return getApartDTOByGPS(searchGpsDTO, apartList, estimateList);
+    }
+
+    private List<ApartDTO> getApartDTOByGPS(SearchGpsDTO searchGpsDTO, List<Apart> apartList, List<Estimate> estimateList) {
+        Comparator<ApartDTO> priceComparator = Comparator.comparing(ApartDTO::getTotalPrice);
+        if ("high".equalsIgnoreCase(searchGpsDTO.getSort())) {
+            priceComparator = priceComparator.reversed();
+        }
+
+        return apartList.stream()
+                .map(apart -> ApartDTO.builder()
+                        .apart(apart)
+                        .estimate(findEstimateForApart(apart, estimateList))
+                        .build())
+                .sorted(priceComparator)
+                .toList();
+    }
+
 }
