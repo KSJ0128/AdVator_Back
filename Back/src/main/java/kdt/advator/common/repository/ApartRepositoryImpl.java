8e2c5a568758e15d8931218e1294a7b716018e5b
KSJ0128 <ksj01128@naver.com>
2024-12-15 03:49:12 +0900

    feat: 지도 건물 조회 - gps : QueryDSL
    지도 건물 조회 - gps API 구현을 위한 QueryDSL을 구현했습니다.
    하버 사인 공식을 통해 좌표 기준 반경 2km 내 아파트 리스트를 조회합니다. 반경 값은 임의로 설정해둔 것으로 추후 변경 가능합니다.

--- Back/src/main/java/kdt/advator/common/repository/ApartRepositoryImpl.java
+++ Back/src/main/java/kdt/advator/common/repository/ApartRepositoryImpl.java
@@ -1,10 +1,11 @@
 package kdt.advator.common.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kdt.advator.common.domain.Apart;
 import kdt.advator.common.domain.QApart;
-import kdt.advator.estimate.domain.QEstimate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -21,7 +22,6 @@ public class ApartRepositoryImpl implements ApartRepositoryCustom{
     @Override
     public List<Apart> findByConditions(String city, String district, String area, List<String> rating, List<String> company) {
         QApart a = QApart.apart;
-        QEstimate e = QEstimate.estimate;
         BooleanBuilder conditions = new BooleanBuilder();
 
         if (city != null && !city.isEmpty())
@@ -46,6 +46,37 @@ public class ApartRepositoryImpl implements ApartRepositoryCustom{
 
         log.info("list size : {}", resultList.size());
         return new ArrayList<>(resultList);  // 중복 제거를 위해 Set으로 변환
+    }
+
+    @Override
+    public List<Apart> findByGpsXAndGpsY(Double gpsX, Double gpsY, List<String> rating, List<String> company) {
+        QApart a = QApart.apart;
+        // 지구 반경 (km)
+        Double earthRadius = 6371.0;
+        // 고정 반경 값 설정 (1km)
+        Double radius = 2.0;
+
+        // Haversine 공식 구현
+        NumberExpression<Double> distanceExpression = Expressions.numberTemplate(Double.class,
+                "{0} * acos(cos(radians({1})) * cos(radians({2})) * cos(radians({3}) - radians({4})) + sin(radians({1})) * sin(radians({2})))",
+                earthRadius, gpsY, a.gpsY, a.gpsX, gpsX);
+
+        // 조건 빌더 생성
+        BooleanBuilder conditions = new BooleanBuilder();
+        conditions.and(distanceExpression.loe(radius)); // 반경 조건 추가
+
+        if (rating != null && !rating.isEmpty()) {
+            conditions.and(ratingEquals(rating));
+        }
+
+        if (company != null && !company.isEmpty()) {
+            conditions.and(companyEquals(company));
+        }
+
+        // QueryDSL 쿼리
+        return queryFactory.selectFrom(a)
+                .where(conditions)
+                .fetch();
     }
 
     private BooleanBuilder roadAddressContains(String value) {
