6321d6d0734017b0143a1d0918635193095d7547
KSJ0128 <ksj01128@naver.com>
2024-12-15 01:22:49 +0900

    refactor: 지도 건물 조회 - 주소 : 필터링 조건 수정
    지도 건물 조회 - 주소 API 필터링 조건을 수정했습니다.
    rating, company가 중복 선택이 가능하도록 변경했습니다.

--- Back/src/main/java/kdt/advator/search/dto/SearchApartDTO.java
+++ Back/src/main/java/kdt/advator/search/dto/SearchApartDTO.java
@@ -3,6 +3,8 @@ package kdt.advator.search.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Schema(description = "지도 건물 조회 - 주소 DTO")
 public class SearchApartDTO {
@@ -19,8 +21,8 @@ public class SearchApartDTO {
     private String sort;
 
     @Schema(description = "평점 기준", example = "A")
-    private String rating;
+    private List<String> rating;
 
     @Schema(description = "광고 업체명", example = "포커스 미디어 코리아")
-    private String company;
+    private List<String> company;
 }
